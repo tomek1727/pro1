f53c6ae86a635ac30c60fbffede187370fee7317
tomek1727 <tomas1727@wp.pl>
2020-06-25 19:27:11 +0200

    syf brud i smród

--- klatka.rb
+++ klatka.rb
@@ -4,17 +4,21 @@ class Klatka
     @pojemnosc = pojemnosc
     @sprzata = sprzata
     @zwierzeta = []
+    @brud = 0
   end
 
   def dodajzwierze(zwierze)
+     @brud += 2
      @zwierzeta.length <= @pojemnosc ? @zwierzeta << zwierze : puts "Brak miejsca w klatce"
   end
 
   def info()
-    puts "Numer: #{@numer}  Zwierząt: #{@zwierzeta.length}/#{@pojemnosc}"
+    @brud += 2
+    puts "Numer: #{@numer}  Zwierząt: #{@zwierzeta.length}/#{@pojemnosc}  Brud: #{@brud}"
   end
 
   def infozwierzeta()
+    @brud += 2
     puts "\n\nKlatka nr: #{@numer}"
     @zwierzeta.each {|x| x.info()}
   end
---
+++ zoo.rb
@@ -0,0 +1,5 @@
+class Zoo
+  def initialize()
+    @pracownicy = []
+    @klatki = []
+  end
